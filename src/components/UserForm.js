aea42620d8626ba1b1911b08e391ac3395fe5678
Meghna Srivastava <meghna.srivastava@auto1.com>
2019-06-03 22:16:24 +0200

    created root userform component

---
+++ src/components/UserForm.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+
+class UserForm extends Component {
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        occupation: '',
+        city: '',
+        bio: '',
+    }
+
+    // Proceed to next step
+    nextStep = () => {
+        const { step } = this.state;
+
+        this.setState({ step: step + 1 });
+    }
+
+    // Go back to prev step
+    prevStep = () => {
+        const { step } = this.state;
+
+        this.setState({ step: step - 1 });
+    }
+
+    // handle fields change
+    handleChange = input => e => {
+        this.setState({ [input]: e.target.value });
+    }
+
+    render() {
+        const {
+            step,
+            firstName,
+            lastName,
+            email,
+            city,
+            bio,
+            occupation
+        } = this.state;
+
+        const values = {
+            firstName,
+            lastName,
+            email,
+            city,
+            bio,
+            occupation
+        }
+
+        switch (step) {
+            case 1:
+                return (
+                    <FormUserDetails
+                        nextStep={this.nextStep}
+                        handleChange={this.handleChange}
+                        values={values}
+                    />
+                )
+            case 2:
+                return <h1>FormPersonalDetails</h1>
+            case 3:
+                return <h1>Confirm</h1>
+            case 4:
+                return <h1>Success</h1>
+        }
+    }
+}
+
+export default UserForm
